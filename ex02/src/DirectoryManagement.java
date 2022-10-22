32bcc1fe2c8a063d2fe55c0ed37df0f07fdb01f9
wabathur <safbul@gmail.com>
2022-10-22 15:44:14 +0300

    ex02 fixed

--- ex02/src/DirectoryManagement.java
+++ ex02/src/DirectoryManagement.java
@@ -4,7 +4,6 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardCopyOption.*;
 
 public class DirectoryManagement {
     private Path currentDirectory;
